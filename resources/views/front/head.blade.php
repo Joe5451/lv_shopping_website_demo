4e5b928b62adcae9161b7eac78cc24cf26c95107
Joe <skybluejoe123@gmail.com>
2022-06-14 08:07:50 +0800

    modify product.list

--- resources/views/front/head.blade.php
+++ resources/views/front/head.blade.php
@@ -31,8 +31,7 @@
             <a href="{{ route('news.list') }}" class="main_header_link" menu-id="2">最新消息</a>
         </div>
         <div class="main_header_link_wrap">
-            <!-- <a href="{{ route('product.first_list') }}" class="main_header_link" menu-id="3">購物商城</a> -->
-            <a href="{{ route('product.list', [null, null]) }}" class="main_header_link" menu-id="3">購物商城</a>
+            <a href="{{ route('product.first_list') }}" class="main_header_link" menu-id="3">購物商城</a>
         </div>
         <div class="main_header_link_wrap">
             <a href="{{ route('contact') }}" class="main_header_link"  menu-id="4">聯絡我們</a>
